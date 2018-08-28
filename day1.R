8b0db4b85f0f4830ee4174c44ede680749a654ac
Aji John <aji.john@gmail.com>
2018-08-28 11:55:46 -0700

    Notes from day 1

---
+++ day1.R
@@ -0,0 +1,73 @@
+# We will be using the R script
+
+#use command-enter(return) to run the line(keyboard shortcut)
+getwd()
+
+#keyboard shortcut
+
+#carat - shortcut
+# press escape to leave the open +
+# stackoverflow -
+
+# how do you assign
+a= 5
+b <- 5
+c <- 5
+
+( weight_kg <- 6)
+
+(2.2 * weight_kg)
+
+
+#functions in R
+# describe built-in functions
+
+#difference between vectors and lists
+
+#vector must contain the same type
+
+animals <- c('mouse', 'rat',"dog")
+animals_list <- list(1,'mouse', 'rat',"dog")
+
+#length of list and vectors
+length(animals)
+length(animals_list)
+
+
+#subsetting the vectors, talk about -1, -2
+#
+
+# why is it forcing to number ?
+# vectors to a single datatype
+num_logical <- c(1,2,3,TRUE)
+char_logical <-  c('1','2','3',TRUE)
+
+
+combined_logical <- c(num_logical, char_logical)
+
+
+#subsetting by indices
+
+
+# subset by filtering
+weight_kg <- c(87,66,23,55,99,199)
+#subsetting by conditions
+# returns the logical vectors
+weight_kg[weight_kg < 60]
+
+
+# or and and operators
+weight_kg[weight_kg < 60 | weight_kg <90]
+#[1] 87 66 23 55
+
+#order is umportant
+weight_kg[(weight_kg < 60 & weight_kg <90)]
+# [1] 23 55
+
+weight_kg[(weight_kg < 90 & weight_kg <60)]
+
+#animals in 
+animals[animals %in% c('rat','dog')]
+animals[!(animals  %in% c('rat','dog'))]
+#handling NAs
+# reprcussions of NAs
